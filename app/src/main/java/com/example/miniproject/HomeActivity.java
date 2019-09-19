b30c74e15c39ff8d472b59b0452f79c99436285d
ridh7 <ridhwik712000@gmail.com>
2019-09-19 22:41:06 +0530

    user data added w.r.t. uid to realtime database

--- app/src/main/java/com/example/miniproject/HomeActivity.java
+++ app/src/main/java/com/example/miniproject/HomeActivity.java
@@ -10,7 +10,7 @@ import android.widget.VideoView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 7000;
+    private static int SPLASH_TIME_OUT = 0000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
